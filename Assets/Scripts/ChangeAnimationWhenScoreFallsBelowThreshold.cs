d51c0256444ace2bb8e2728129203afa9a8f7bc2
Derrick Barra <gambitgamesllc@gmail.com>
2025-08-06 11:08:08 -0400

    Added safeguard to prevent bug with the changeToProgress value

--- Assets/Scripts/ChangeAnimationWhenScoreFallsBelowThreshold.cs
+++ Assets/Scripts/ChangeAnimationWhenScoreFallsBelowThreshold.cs
@@ -23,6 +23,47 @@ public class ChangeAnimationWhenScoreFallsBelowThreshold : MonoBehaviour, INeuro
 
     #endregion
 
+    #region PUBLIC - START
+
+    /// <summary>
+    /// Unity lifecycle function
+    /// </summary>
+    //------------------------------//
+    public void Start()
+    //------------------------------//
+    {
+        PreventChangeToProgressValueBeingAboveThreshold();
+
+    } //END Start Method
+
+    #endregion
+
+    #region PRIVATE - PREVENT CHANGE TO PROGRESS VALUE BEING ABOVE THRESHOLD
+
+    /// <summary>
+    /// Prevents the change to progress value from accidentally being set to a value higher then the threshold.
+    /// If this was allowed to occur, you could enter a state that impossible to leave
+    /// </summary>
+    //-------------------------------------------------------------------//
+    private void PreventChangeToProgressValueBeingAboveThreshold()
+    //-------------------------------------------------------------------//
+    {
+
+        if(NeuroGuideAnimationExperience.system == null)
+        {
+            return;
+        }
+
+        if(changeToProgress > NeuroGuideAnimationExperience.system.options.threshold)
+        {
+            changeToProgress = NeuroGuideAnimationExperience.system.options.threshold;
+            Debug.LogWarning( "ChangeAnimationWhenScoreFallsBelowThreshold.cs PreventChangeToProgressValueBeingAboveThreshold() The changeToProgress value cannot be above the threshold. Forcibly setting the changeToProgress value to the same value as the threshold." );
+        }
+
+    } //END PreventChangeToProgressValueBeingAboveThreshold Method
+
+    #endregion
+
     #region PUBLIC - NEUROGUIDE - ON ABOVE THRESHOLD
 
     public void OnAboveThreshold()
